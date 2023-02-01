9c19407292ff09d1c665346a09c4b0e86015ee77
Lorenzo Garuti <garuti.lorenzo@gmail.com>
2023-02-01 11:30:45 +0100

    Upgrade to Ubunbu 22.04

--- vars.tf
+++ vars.tf
@@ -38,7 +38,8 @@ variable "PATH_TO_PRIVATE_KEY" {
 
 variable "os_image_id" {
   type    = string
-  default = "ocid1.image.oc1.eu-zurich-1.aaaaaaaag2uyozo7266bmg26j5ixvi42jhaujso2pddpsigtib6vfnqy5f6q" # Canonical-Ubuntu-20.04-aarch64-2022.01.18-0
+  # default = "ocid1.image.oc1.eu-zurich-1.aaaaaaaag2uyozo7266bmg26j5ixvi42jhaujso2pddpsigtib6vfnqy5f6q" # Canonical-Ubuntu-20.04-aarch64-2022.01.18-0
+  default = "ocid1.image.oc1.eu-zurich-1.aaaaaaaadtfctegquwzdim6vaz32xvaxy74ptji4gwwohnylrz57moilieua" # Canonical-Ubuntu-22.04-aarch64-2022.11.06-0
 }
 
 variable "compute_shape" {
