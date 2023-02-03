4146474334301b2c2aaa082c0b627480d113f9a1
Lorenzo Garuti <garuti.lorenzo@gmail.com>
2023-02-03 15:50:26 +0100

    Cleanup

--- locals.tf
+++ locals.tf
@@ -1,4 +1,5 @@
 locals {
+  public_lb_ip = [for interface in oci_network_load_balancer_network_load_balancer.k8s_public_lb.ip_addresses : interface.ip_address if interface.is_public == true]
   tags = {
     environment      = "${var.environment}"
     provisioner      = "terraform"
