260fa37a7c2a51173d94a5f0d3094e8abbd5596a
Lorenzo Garuti <garuti.lorenzo@gmail.com>
2023-02-07 11:52:09 +0100

    Updated example

--- example/main.tf
+++ example/main.tf
@@ -1,41 +1,52 @@
-variable "compartment_ocid" {
-
-}
-
-variable "tenancy_ocid" {
-
-}
-
-variable "user_ocid" {
-
-}
-
-variable "fingerprint" {
-
-}
-
-variable "private_key_path" {
-
-}
-
+variable "compartment_ocid" {}
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
 variable "region" {
+  default = "eu-zurich-1"
+}
+variable "public_key_path" {
+  default = "~/.ssh_mac/id_rsa.pub"
+}
+variable "availability_domain" {
   default = "<change_me>"
+}
+variable "my_public_ip_cidr" {
+  default = "<change_me>"
+}
+variable "environment" {
+  default = "staging"
+}
+variable "install_longhorn" {
+  default = true
+}
+variable "install_nginx_ingress" {
+  default = true
+}
+variable "install_certmanager" {
+  default = true
+}
+variable "certmanager_email_address" {
+  default = "<change_me>"
+}
+variable "expose_kubeapi" {
+  default = true
 }
 
 module "k8s_cluster" {
-  PATH_TO_PUBLIC_KEY     = "<change_me>"
-  PATH_TO_PRIVATE_KEY    = "<change_me>"
-  PATH_TO_PUBLIC_LB_CERT = "<change_me>"
-  PATH_TO_PUBLIC_LB_KEY  = "<change_me>"
-  region                 = var.region
-  availability_domain    = "<change_me>"
-  compartment_ocid       = var.compartment_ocid
-  my_public_ip_cidr      = "<change_me>"
-  environment            = "staging"
-  uuid                   = "<change_me>"
-  install_longhorn       = true
-  install_nginx_ingress  = true
-  source                 = "github.com/garutilorenzo/k8s-oci-cluster"
+  public_key_path           = var.public_key_path
+  region                    = var.region
+  availability_domain       = var.availability_domain
+  compartment_ocid          = var.compartment_ocid
+  my_public_ip_cidr         = var.my_public_ip_cidr
+  environment               = var.environment
+  install_longhorn          = var.install_longhorn
+  install_nginx_ingress     = var.install_nginx_ingress
+  install_certmanager       = var.install_certmanager
+  certmanager_email_address = var.certmanager_email_address
+  expose_kubeapi            = var.expose_kubeapi
+  source                    = "../"
 }
 
 output "k8s_servers_ips" {
